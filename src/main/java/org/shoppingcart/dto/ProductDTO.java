f6c0cda6b5dd72b6dba22b2e9c730c62d2b89094
AbiramiSuga <rami.suga3@gmail.com>
2025-01-16 14:33:38 +0530

    ProductDTO implemented

---
+++ src/main/java/org/shoppingcart/dto/ProductDTO.java
@@ -0,0 +1,13 @@
+package org.shoppingcart.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+}
